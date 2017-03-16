0bb33be7b6bff5c2b450bb27e072def2cd3a5a77
shsunix <shsunix@gmail.com>
2017-03-16 13:38:48 +0000

    created directory

--- cookbooks/nginx/recipes/default.rb
+++ cookbooks/nginx/recipes/default.rb
@@ -10,6 +10,17 @@ package 'nginx' do
   action :install
 end
 
+directory '/var/www/nginx-default' do
+  owner 'root'
+  group 'root'
+  mode '0755'
+  action :create
+end
+
+template "/var/www/nginx-default/index.html" do
+ source "index.html"
+ mode "0644"
+end
 
 service 'nginx' do
   action [ :enable, :start ]
