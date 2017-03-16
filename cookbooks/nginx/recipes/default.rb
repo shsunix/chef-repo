d9330b04ca1471a0ca2a254201ed32a8dfb91f23
shsunix <shsunix@gmail.com>
2017-03-16 13:55:54 +0000

    last test

--- cookbooks/nginx/recipes/default.rb
+++ cookbooks/nginx/recipes/default.rb
@@ -10,19 +10,13 @@ package 'nginx' do
   action :install
 end
 
-directory '/var/www/nginx-default' do
-  owner 'root'
-  group 'root'
-  mode '0755'
-  action :create
-end
+service 'nginx' do
+  action [ :enable, :start ]
+end 
 
-template "/var/www/nginx-default/index.html" do
+cookbook_file "/var/www/index.html" do
  source "index.html"
  mode "0644"
 end
 
-service 'nginx' do
-  action [ :enable, :start ]
-end
 
