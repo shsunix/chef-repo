6faa746ca7c3b9783fa1cd0ae9b1ae5c33f72a1d
shsunix <shsunix@gmail.com>
2017-03-17 09:00:27 +0000

    new cookbook

---
+++ cookbooks/cookie/recipes/default.rb
@@ -0,0 +1,14 @@
+i#
+# Cookbook Name:: cookie
+# Recipe:: default
+#
+# Copyright 2017, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+directory '/etc/apache2' do
+  owner 'root'
+  group 'root'
+  mode '0755'
+  action :create
+end
