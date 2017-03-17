57c62667a1f1d5e502e8b87095e7d7361029651b
shsunix <shsunix@gmail.com>
2017-03-17 11:48:54 +0000

    Made changes

--- cookbooks/nginx/recipes/default.rb
+++ cookbooks/nginx/recipes/default.rb
@@ -9,6 +9,12 @@ directory '/var/test' do
   owner 'root'
   group 'root'
   mode '0755'
-  recursive true
   action :create
+end
+
+directory '/var/test1' do
+  owner 'root'
+  group 'root'
+  mode '0755'
+  recursive true
 end
