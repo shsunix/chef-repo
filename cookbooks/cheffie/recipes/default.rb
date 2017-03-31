51db1732f04dd08be34d4602c5491d10b7eb36a3
shsunix <shsunix@gmail.com>
2017-03-31 11:06:08 +0000

    directories with permissions

--- cookbooks/cheffie/recipes/default.rb
+++ cookbooks/cheffie/recipes/default.rb
@@ -14,6 +14,27 @@ service 'apache2' do
   action [ :enable, :start ]
 end
 
+directory '/var' do
+  owner 'root'
+  group 'root'
+  mode '0755'
+  action :create
+end
+
+directory '/var/www' do
+  owner 'root'
+  group 'root'
+  mode '0755'
+  action :create
+end
+
+directory '/var/www/html' do
+  owner 'root'
+  group 'root'
+  mode '0755'
+  action :create
+end
+
 cookbook_file "/var/www/html/index.html" do
   source "index.html"
   mode "0644"
